c5b6d187048d8c81cb12941d685373a658c9b0c1
Fabien de Maestri <fabien.demaestri@gmail.com>
2016-10-01 00:25:16 +0200

    set redis url in heroku

--- src/server/cache/index.js
+++ src/server/cache/index.js
@@ -3,7 +3,7 @@
 
 const Promise = require('bluebird');
 const redis = require('redis');
-const URI = require('urijs');
+const url = require('url');
 
 Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
@@ -11,17 +11,17 @@ Promise.promisifyAll(redis.Multi.prototype);
 module.exports = {
   createClient() {
     let client;
-    if (process.env.REDISTOGO_URL) {
-      const redisURL = new URI(process.env.REDISTOGO_URL);
-      client = redis.createClient(redisURL.port(), redisURL.hostname());
-      client.auth(redisURL.authority().split(':')[1]);
-      return client;
+    try {
+      if (process.env.REDISTOGO_URL) {
+        const redisUrl = url.parse(process.env.REDISTOGO_URL);
+        client = redis.createClient(redisUrl.port, redisUrl.hostname);
+        client.auth(redisUrl.auth.split(':')[1]);
+        return client;
+      }
+      return redis.createClient();
+    } catch (e) {
+      console.log('ERROR :', e);
     }
-    client = redis.createClient();
-    client.on('error', (err) => {
-      console.log("errr", err);
-    });
-    return client;
   },
   save(client, key) {
     return (value) => client.setAsync(key, JSON.stringify(value)).then(() => value);
