ef043843bdca7efecad088978a43fd851a394147
Fabien de Maestri <fabien.demaestri@gmail.com>
2017-01-15 19:28:10 +0100

    move index.js to root

--- index.js
+++ index.js
@@ -4,19 +4,18 @@
 const express = require('express');
 const path = require('path');
 
-const cors = require('./cors');
-const routes = require('./routes');
+const cors = require('./server/cors');
+const routes = require('./server/routes');
 
 const app = express();
 
 app.use(cors);
-app.use('/', routes);
-app.get('/', (req, res) => {
-  return res.sendFile(path.join(__dirname, '../dist'));
-});
-app.use(express.static('app'));
 app.use(express.static('img'));
 app.use(express.static('dist'));
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+app.use(routes);
 
 const listener = app.listen(process.env.PORT || 3000);
 console.log('Server running on port', listener.address().port);
