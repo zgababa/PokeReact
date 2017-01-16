8de3e4de3cc0f05aff0d5a3a3c6c4ce2bcff4d24
Fabien de Maestri <fabien.demaestri@gmail.com>
2017-01-16 23:03:15 +0100

    Add port env

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.get('/', (req, res) => {
 });
 app.use(routes);
 app.listen(config.app.port, () => {
-  console.log('Server running on port', config.docker.port);
+  console.log('Server running on port', config.app.port);
 });
